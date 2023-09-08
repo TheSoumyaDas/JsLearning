6d73bc498ee7d0715f993f621b9905f4ae1383af
soumya-sysbioz <soumya@sysbioz.com>
2023-09-08 18:04:20 +0530

    Added the Files and tested the git commands

---
+++ dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+//alert is used in browser not in node.js
+
+// Code Readability should be high
+// Javascript OG docs --> https://tc39.es/ecma262/#sec-intro
+
+// Datatypes
+
+//Primitive DataTypes
+
+/*
+Number => 2 to power 53
+bigint => bigger set of number's
+string => "Soumya", 'Soumyaranjandas'
+boolean => true/false
+null => standalone value
+undefined => values are not set
+symbol => unique indetifier for objects and functions
+*/
+
+//object
+//Null is considered as Object
+//To identify the the datatype you can use **typeof**
+console.log(typeof null);
