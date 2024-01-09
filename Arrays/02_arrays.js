142f97e0ead201a3bde1588dbe6de26a4de1e7b6
isoumyadas <sammy8das@gmail.com>
2024-01-09 09:30:33 +0530

    added arrays conecpts

--- Arrays/02_arrays.js
+++ Arrays/02_arrays.js
@@ -22,6 +22,7 @@ console.log("Spread_Operator::: ", all_heros);
 // Flat method helps to flat all the elements present in an array creates a new array with all sub-array elements concatenated
 
 const allArray = [1, 2, 3, [2, 3, 4, 5], [23, 4, 5]];
+console.log("Old AllArray:::", allArray);
 console.log(allArray.flat(Infinity));
 
 // when you do data scraping, when scrap data from another websites it comes in another format like nodelist, objects, strings but we need array and also we need to loop an array
@@ -29,7 +30,7 @@ console.log(allArray.flat(Infinity));
 
 //asking if it's an array
 console.log(Array.isArray("soumya"));
-console.log(Array.from("soumya"));
+console.log(Array.from("soumya")); // this converts this into individual seperation.
 
 // But when you pass object as element
 console.log(Array.from({ name: "Soumya" })); // Then this show empty array [], because it ask's whether to take key or value we have specific it
--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -8,30 +8,32 @@ const heros = new Array("hello", 7, 8);
 console.log(heros);
 console.log(heros[0]);
 
-// Arrays are the shallow copy and deep copy
+// Arrays are the shallow copy and deep copy // shallow copy mean share's same reference point and deep don't!! (like stack and heap)
 
 //Arrays Methods
-let arrays = [0, 1, 2, 3, 4, 5];
-//how to add the elements in an existing array
-arrays.push(6, 9);
+let arrays = [0, 1, 2, 55, 3, 4, 5, 77];
+//how to add the elements in an existing array // you can put only one element while push not like this (7,8) only this (7)
+arrays.push(99);
+console.log("Push:", arrays);
 
 // pop's helps to remove the last element of the array
-arrays.pop();
+arrays.pop(); // there should no parameter while using pop
+console.log("Pop:", arrays);
 
 //unshift helps to add the value in elements at starting, but is unoptimized so use it wisely
 arrays.unshift(9);
+console.log("unshift", arrays);
 
 // shift helps to remove the value in an array at starting
 arrays.shift();
-
-console.log(arrays);
+console.log("shift", arrays);
 
 // includes and indexOf {Includes gives boolean value if the element exists or not} {indexOf gives index of the exisitng array it gives the position of the element}
 console.log(arrays.includes(5));
 console.log(arrays.includes(8));
 
-console.log(arrays.indexOf(5));
-console.log(arrays.indexOf(9));
+console.log(arrays.indexOf(77));
+console.log(arrays.indexOf(55));
 
 //join() method
 //join method helps to join all the array elements bind them and convert them into string!
@@ -45,7 +47,10 @@ console.log(ice);
 console.log(ice.slice(2, 4)); // in this it will start from 2nd index and end on 3rd index
 
 // and splice removes the elements in an array and it also change the elements of the array => the remaining elements
-console.log(ice.splice(2, 4));
+console.log(ice.splice(2, 4)); // splice change the original array , so when we splice the value then it removes the splice part in the original array : for ex
 console.log(ice);
 
+const something = [1, 22, 43, 34, 15, 36, 67, 88];
+console.log("SPLICE:::", something.splice(2, 6)); // it includes the last index too => also this say's is it will start from the 2nd index and splice till 6th value from their, like it will start from 43 and end till it got 6th value
+console.log("OG::", something); // og value got changed
 // Learn more about slice and splice
