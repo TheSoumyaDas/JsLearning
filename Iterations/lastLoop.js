d3f140583663f17021301a823d08b0575da76dc6
isoumyadas <sammy8das@gmail.com>
2024-04-27 15:48:08 +0530

    Added new lines of code for Map, Reduce, Filter

--- Iterations/lastLoop.js
+++ Iterations/lastLoop.js
@@ -1,7 +1,7 @@
 const coding = ["cpp", "js", "gatsby", "react", "next", "node"];
 
 const newArr = coding.forEach((items) => console.log(items));
-console.log(newArr);
+console.log("newArr", newArr);
 
 // when we store this in a variable, but forEach doesn't return anything when we try to return it will show undefined
 let lastestLoop = [];
@@ -21,7 +21,7 @@ const numbs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // here we have to pass callback
 const val = numbs.filter((nums) => nums > 4);
 console.log(val);
-console.log("oldArr::", numbs);
+console.log("oldArr::", numbs); // filter doesn't change the og array
 
 // when we start a scope we have return something
 
@@ -29,6 +29,22 @@ const num = numbs.filter((val) => {
   return val > 7; // so when where you start a scope or create a scope always try to return it or else it will not return anything
 });
 console.log(num);
+
+const words = [
+  "Ok",
+  "Soumya",
+  "Narendra",
+  "Jatin",
+  "Nitin",
+  "Nilesh",
+  "Nilesh",
+];
+
+const filterWords = words.filter((word) => {
+  return word.length > 5;
+});
+
+console.log("filterWords:::>", filterWords);
 
 // if we want to check condition on forEach
 const newNumArray = [];
@@ -111,6 +127,41 @@ allNumbers.forEach((item) => {
 });
 console.log(newAllNums);
 
+//Another Example you can use the map:
+const kvArray = [
+  { key: 1, value: 10 },
+  { key: 2, value: 20 },
+  { key: 3, value: 30 },
+];
+
+const reformateedArray = kvArray.map(({ key, value }) => ({ [key]: [value] })); // here square brackets are used to dynamically pass the key from the kvArray.
+console.log("ReformatedArray", reformateedArray);
+
+// Another way
+const products = [
+  { name: "sports car" },
+  { name: "laptop" },
+  { name: "phone" },
+];
+
+const productsWithPrice = products.map((product) => {
+  return { ...product, price: 100 };
+});
+console.log(productsWithPrice);
+
+// parseInt
+const parseINT = ["1", "2", "3"].map(parseInt);
+console.log("ParseINT:>", parseINT);
+/**
+ * Array.prototype.map passes 3 arguments: the element, the index, and the array. The third argument is ignored by parseInt — but not the second one! This is the source of possible confusion.
+ * parseInt("1", 0); // 1
+ * parseInt("2", 1); // NaN
+ * parseInt("3", 2); // NaN
+ */
+
+// Whenever you want to create new array and want loop then you can use map
+// or else you can use ForEach // use ForEach only for looping purpose not for creating new array
+
 // chaining (one or more method can be used)
 const chaining = allNumbers
   .map((item) => item * 10)
@@ -127,7 +178,7 @@ const newReduceVal = reduceNums.reduce((acc, currVal) => {
   console.log(`acc:: ${acc} and currVal:: ${currVal}`);
   return acc + currVal; // so here acc is an empty var which takes value from which we have declared after curly barce (4)
 }, 4);
-console.log(newReduceVal);
+console.log("newReduceVal", newReduceVal);
 
 const optimize = reduceNums.reduce((acc, currVal) => acc + currVal, 1); // works as same as aboveExample // optimize code
 console.log(optimize);
